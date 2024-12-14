a31524f16820fd89d76b7e0471142a05308bb3f3
Ryan Baldwin <rtbaldwin777@gmail.com>
2024-12-14 15:12:35 -0600

    day 12 part 1

--- 2024/day11/day11.go
+++ 2024/day11/day11.go
@@ -38,12 +38,12 @@ func part2(input []string, cache map[cacheval]int) {
 func calculate(input []string, blinks int, cache map[cacheval]int) int {
 	result := 0
 	for _, val := range input {
-		result += applyRules2(val, blinks, cache)
+		result += applyRules(val, blinks, cache)
 	}
 	return result
 }
 
-func applyRules2(val string, blinks int, cache map[cacheval]int) int {
+func applyRules(val string, blinks int, cache map[cacheval]int) int {
 	if blinks == 0 {
 		return 1
 	}
@@ -52,33 +52,18 @@ func applyRules2(val string, blinks int, cache map[cacheval]int) int {
 		return res
 	}
 	if val == "0" {
-		res += applyRules2("1", blinks-1, cache)
+		res += applyRules("1", blinks-1, cache)
 	} else if len(val)%2 == 0 {
 		newNum1, _ := strconv.Atoi(val[:len(val)/2])
 		newStr1 := strconv.Itoa(newNum1)
 		newNum2, _ := strconv.Atoi(val[len(val)/2:])
 		newStr2 := strconv.Itoa(newNum2)
-		res += applyRules2(newStr1, blinks-1, cache) + applyRules2(newStr2, blinks-1, cache)
+		res += applyRules(newStr1, blinks-1, cache) + applyRules(newStr2, blinks-1, cache)
 	} else {
 		num, _ := strconv.Atoi(val)
-		res += applyRules2(strconv.Itoa(num*2024), blinks-1, cache)
+		res += applyRules(strconv.Itoa(num*2024), blinks-1, cache)
 
 	}
 	cache[cacheval{blinks, val}] = res
 	return res
-}
-
-func applyRules(val string) []string {
-	if val == "0" {
-		return []string{"1"}
-	}
-	if len(val)%2 == 0 {
-		newNum1, _ := strconv.Atoi(val[:len(val)/2])
-		newStr1 := strconv.Itoa(newNum1)
-		newNum2, _ := strconv.Atoi(val[len(val)/2:])
-		newStr2 := strconv.Itoa(newNum2)
-		return []string{newStr1, newStr2}
-	}
-	num, _ := strconv.Atoi(val)
-	return []string{strconv.Itoa(num * 2024)}
 }
---
+++ 2024/day12/day12.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"time"
+
+	"github.com/ryanbase/advent-of-code/2024/utils"
+)
+
+type plot struct {
+	row int
+	col int
+}
+
+type region struct {
+	perimiter int
+	plots     []plot
+}
+
+func main() {
+	defer utils.TimeTrack(time.Now())
+	filename := utils.GetFileNameFromArgument()
+	input := utils.ReadInputAsByteMatrix(filename)
+	regions := getRegions(input)
+	part1(regions)
+	// part2(regions)
+}
+
+func part1(regions []region) {
+	defer utils.TimeTrack(time.Now())
+	price := 0
+	for _, region := range regions {
+		price += len(region.plots) * region.perimiter
+	}
+	println(price)
+}
+
+// Part 2 not complete
+func part2(regions []region) {
+	defer utils.TimeTrack(time.Now())
+	price := 0
+	for _, region := range regions {
+		// Find new perimiters
+		price += len(region.plots) * region.perimiter
+	}
+	println(price)
+}
+
+func getRegions(input [][]byte) []region {
+	defer utils.TimeTrack(time.Now())
+	regions := []region{}
+	visited := make(map[plot]struct{})
+	for i, row := range input {
+		for j, _ := range row {
+			if _, ok := visited[plot{i, j}]; !ok {
+				region := getRegion(input, i, j, visited)
+				regions = append(regions, region)
+			}
+		}
+	}
+	return regions
+}
+
+func getRegion(input [][]byte, row int, col int, visited map[plot]struct{}) region {
+	region := region{0, []plot{}}
+	stack := []plot{plot{row, col}}
+	for len(stack) > 0 {
+		curr := stack[0]
+		stack = stack[1:]
+		if _, ok := visited[curr]; ok {
+			continue
+		}
+		visited[curr] = struct{}{}
+		region.plots = append(region.plots, curr)
+		currValue := input[curr.row][curr.col]
+		if curr.row > 0 {
+			if input[curr.row-1][curr.col] == currValue {
+				stack = append(stack, plot{curr.row - 1, curr.col})
+			} else {
+				region.perimiter++
+			}
+		} else {
+			region.perimiter++
+		}
+		if curr.row < len(input)-1 {
+			if input[curr.row+1][curr.col] == currValue {
+				stack = append(stack, plot{curr.row + 1, curr.col})
+			} else {
+				region.perimiter++
+			}
+		} else {
+			region.perimiter++
+		}
+		if curr.col > 0 {
+			if input[curr.row][curr.col-1] == currValue {
+				stack = append(stack, plot{curr.row, curr.col - 1})
+			} else {
+				region.perimiter++
+			}
+		} else {
+			region.perimiter++
+		}
+		if curr.col < len(input[curr.row])-1 {
+			if input[curr.row][curr.col+1] == currValue {
+				stack = append(stack, plot{curr.row, curr.col + 1})
+			} else {
+				region.perimiter++
+			}
+		} else {
+			region.perimiter++
+		}
+	}
+	return region
+}
