8484be7908e19c9615c1471f301822e08f64e6f4
Ryan Baldwin <rtbaldwin777@gmail.com>
2025-12-07 14:50:16 -0600

    2025 day 7 part 2

--- 2025/src/day7/day7.ts
+++ 2025/src/day7/day7.ts
@@ -44,4 +44,41 @@ function part1() {
   return splits;
 }
 
+/**
+ * Note: I got the idea for this solution from this Redit post:
+ * https://www.reddit.com/r/adventofcode/comments/1pgnmou/2025_day_7_lets_visualize/
+ * @returns timelines
+ */
+function part2() {
+  const beams = new Array(rows[0]!.length).fill(undefined).map(() => 0);
+
+  const start = rows[0]?.indexOf("S");
+
+  if (!start) {
+    return 0;
+  }
+
+  beams[start]!++;
+
+  for (let i = 1; i < rows.length; i++) {
+    for (let j = 0; j < rows[i]!.length; j++) {
+      const val = rows[i]![j];
+      if (val === "^" && beams[j]! > 0) {
+        if (j > 0) {
+          beams[j - 1]! += beams[j]!;
+        }
+        if (j < beams.length - 1) {
+          beams[j + 1]! += beams[j]!;
+        }
+        beams[j] = 0;
+      }
+    }
+  }
+
+  return beams.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+}
+
 console.log(part1());
+console.log(part2());
